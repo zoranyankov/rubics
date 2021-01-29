6d86f1e9a4d4bf8c53fb3643cf8e50f9f90c5d33
zoranyankov <zoran_yankov@yahoo.com>
2021-01-29 03:02:25 +0200

    add search

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,20 +3,24 @@ const productService = require('../services/productServices');
 const router = Router();
 
 router.get('/', (req, res) => {
-    const cubes = productService.getAll();
-    console.log(cubes);
-    res.render('home', {cubes});
+    const cubes = productService.getAll(req.query);
+    res.render('home', { cubes });
 })
 router.get('/create', (req, res) => {
     res.render('create');
 })
 router.post('/create', (req, res) => {
     return productService.create(req.body)
-    .then(data => {
-        console.log(data);
-        res.redirect('/products');
-    })
-    .catch(err => console.log('Error : ' + err));
+        .then(data => {
+            console.log(data);
+            res.redirect('/products');
+        })
+        .catch(err => console.log('Error : ' + err));
 })
+router.get('/details/:_id', (req, res) => {
+    const currentCube = productService.getOne(req.params._id)
+    res.render('details', { ...currentCube });
+})
+
 
 module.exports = router;
--- services/productServices.js
+++ services/productServices.js
@@ -7,8 +7,22 @@ function create(data) {
     return productData.create(newData);
 }
 
-function getAll() {
-    return productData.getAll();
+function getAll(query) {
+    console.log(query);
+    const {search, from, to} = query;
+    let products = productData.getAll();
+
+    if(search) {
+        products = products.filter(x => x.name.toLocaleLowerCase().includes(search));
+    }
+    if(from) {
+        products = products.filter(x => x.difficultyLevel >= from);
+    }
+    if(to) {
+        products = products.filter(x => x.difficultyLevel <= to);
+    }
+
+    return products;
 }
 
 function getOne(_id) {
