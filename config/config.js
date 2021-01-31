8132c4dab75e4005b4460e956713e6d34b2d5a84
zoranyankov <zoran_yankov@yahoo.com>
2021-02-01 01:41:27 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -5,6 +5,7 @@ module.exports = {
     },
     production: {
         PORT: process.env.PORT || 80,
-        DB_PATH: 'mongodb+srv://<user>:<pass>@softuni.vko2t.mongodb.net/<db-name>?retryWrites=true&w=majority' || 'mongodb://localhost:27017/CubesDB'
+//         DB_PATH: 'mongodb+srv://<user>:<pass>@softuni.vko2t.mongodb.net/<db-name>?retryWrites=true&w=majority' || 'mongodb://localhost:27017/CubesDB'
+        DB_PATH: 'write your own Mongo connection-path here!!!'
     }
 } 
